1188e5dbdc1dfa7329d043637b272628ac00e0d8
ylc <ylcgoogle@163.com>
2018-08-13 15:45:15 +0800

    route

--- myself/src/router/index.js
+++ myself/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home.vue'
+import Home from '@/views/Home.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: Home
+		},{
+			path: '/',
+			name: 'About',
+			component: Home
+		},
+	]
 })
