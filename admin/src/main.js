4120b056c871d4373ed6829d0462931eb042297b
Summer <ylcgoogle@163.com>
2018-12-10 22:27:24 +0800

    代码片段高亮

--- admin/src/main.js
+++ admin/src/main.js
@@ -9,8 +9,17 @@ import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 import * as filters from './libs/filters';
 import { Loading } from "element-ui"
+import hljs from 'highlight.js';
+import 'highlight.js/styles/googlecode.css' //样式文件
 Vue.use(Loading.directive);
-
+Vue.directive('highlight',function (el) {
+    let blocks = el.querySelectorAll('pre code');
+    setTimeout(() =>{
+        blocks.forEach((block)=>{
+        hljs.highlightBlock(block)
+        })
+    }, 200)
+})
 
 Object.keys(filters).forEach(key => {
     Vue.filter(key, filters[key])
